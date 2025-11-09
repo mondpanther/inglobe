acb5b471d6f25f6d1ef2ab9f12527d183ef46687
mondpanther <ralf.martin@gmail.com>
2025-11-09 04:06:41 +0000

    rr

---
+++ prepdata.R
@@ -0,0 +1,8 @@
+# Prep data..
+
+
+
+
+if (Sys.info()["nodename"] == "PCACL-9RVSQ7") {
+  datad="C:\\Users\\rmartin7\\OneDrive - WBG\\projects\\PRINZGlobal\\imperial_college_visualization\\01_data"
+}
